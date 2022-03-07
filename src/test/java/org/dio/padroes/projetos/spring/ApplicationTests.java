c9addd7edc94ae2169c2f69ad4fd0855606b45c4
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-03-07 00:13:29 -0300

    stated spring design patterns

---
+++ src/test/java/org/dio/padroes/projetos/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dio.padroes.projetos.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
